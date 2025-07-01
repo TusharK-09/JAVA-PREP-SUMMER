0da6254ea885b8ea3866578d1363b5a2890c1f10
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-07-01 17:10:21 +0530

    Time: 0 ms (100%), Space: 45.5 MB (81.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,39 +1,38 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
        
-        ArrayList<Integer> row_pos =  new ArrayList<>();
-        ArrayList<Integer> col_pos =  new ArrayList<>();
-        int length = matrix.length;
-        int elem ;
-        int m = matrix.length;
-        int n = matrix[0].length;
+       int m = matrix.length;
+       int n = matrix[0].length;
 
-        //detecting the postion of zero element
-        for(int i = 0;  i < m; i++){
-            for(int j=0; j < n; j++){
-                elem = matrix[i][j];
+       boolean[] row_to_zero = new boolean[m];
+       boolean[] col_to_zero = new boolean[n];
 
-                if(elem == 0) {
-                    row_pos.add(i);
-                    col_pos.add(j);
-                }
+       //detect zero and marking
+       for(int i=0; i < m; i++){
+        for(int j=0; j < n; j++){
+            if(matrix[i][j] == 0){
+                row_to_zero[i] = true;
+                col_to_zero[j] = true;
             }
         }
+       }
 
-        //assigning zeros
-        
-
-            for(int i = 0;  i < m; i++){
-            for(int j=0; j < n; j++){
-               if(row_pos.contains(i) || col_pos.contains(j)){
+       //marking the row zero first
+       for(int i=0; i<m; i++){
+        if(row_to_zero[i]){
+            for(int j=0; j<n; j++){
                 matrix[i][j] = 0;
-               }
             }
-        
-
         }
+       }
 
-
-
+       //marking the column zero now
+       for(int j=0; j<n; j++){
+        if(col_to_zero[j]){
+            for(int i=0; i<m; i++){
+                matrix[i][j]=0;
+            }
+        }
+       }
     }
 }
